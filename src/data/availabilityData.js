7607485444daa22bcd9363b81305acf157de6c54
Stephen Gbolagade <gbolabyte222@gmail.com>
2023-12-05 07:25:33 +0100

    Updated links and stacks

--- src/data/availabilityData.js
+++ src/data/availabilityData.js
@@ -3,18 +3,20 @@ export const availableData = [
       title: "Frontend Enginneering",
       content: "With over 4 years experience, and passion for creating user-centric web experiences and a strong technical background in HTML, CSS, JavaScript, and popular frameworks such as React and VueJs, I bring a unique combination of technical expertise and design acumen to every project.",
       stacks: [
+        {item: "JavaScript / TypeScript"},
+        {item: "Python"},
         {item: "React.js"},
         {item: "Next.js"},
         {item: "Vue.js"},
-        {item: "TypeScript"},
         {item: "GraphQL"},
         {item: "REST API"},
         {item: "Material UI"},
         {item: "Sanity CMS"},
+        {item: "Strapi"},
         {item: "Tailwind"},
         {item: "Bootstrap"},
-        {item: "SCSS"},
-        {item: "Styled-component"}
+        {item: "SASS / SCSS"},
+        {item: "Styled-component"},
         ]
     },
 
